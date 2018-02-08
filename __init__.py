ca96b7ca6721ded74ee46ce6278b6119967ab66a
Anton Veretennikov <stigmato135@gmail.com>
2018-02-08 14:59:51 +0300

    2

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import telepot, time, subprocess
+from telepot.namedtuple import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+from botpy import  last_letter
+from syllables import syllables
+
+def handle(msg):
+	content_type, chat_type, chat_id = telepot.glance(msg)
+
+	if (content_type == 'text'):
+		command = msg['text']
+		bot.sendMessage(chat_id, last_letter(command))				
+
+f = open('token', 'r')
+token=f.read()
+f.close()
+print (token)
+bot = telepot.Bot(token)
+
+bot.message_loop(handle)
+
+while 1:
+    time.sleep(20)
--- botpy.py
+++ botpy.py
@@ -1,82 +1,40 @@
+from syllables import syllables
+
+
 vowels="аАоОиИеЕёЁэЭыЫуУюЮяЯ"
 vowels_list=['а','и','е','ё','о','у','ы','э','ю','я',]
 def last_letter(word):
-	syllables=[]
-	c=0
-	k=0
-
-	while k<len(word):
-		if any(word[k] in s for s in vowels_list):
-			l=False	
-			k+=1
-			while l==False and k<len(word):
-				if any(word[k] in s for s in vowels_list):
-					syllables.append(word[c:k])
-					c=k					
-				if any(word[k] in s for s in vowels_list) and k==len(word)-1:
-					c=k
-					n=True
-					while n==True:
-						if not any(word[c] in s for s in vowels_list):
-							c-=1
-						else:
-							if not any(word[c-1] in s for s in vowels_list):
-								syllables[len(syllables)-1]=syllables[len(syllables)-1][0:-1]
-								syllables.append(word[c-1:])
-							else:
-								syllables.append(word[c:])				
-							n=False
-				if not any(word[k] in s for s in vowels_list) and k==len(word)-1:
-					c=k
-					n=True
-					while n==True:
-						if not any(word[c] in s for s in vowels_list):
-							c-=1
-						else:
-							if not any(word[c-1] in s for s in vowels_list):
-								syllables[len(syllables)-1]=syllables[len(syllables)-1][0:-1]
-								syllables.append(word[c-1:])
-							else:
-								syllables.append(word[c:])				
-							n=False
-				k+=1		
-		k+=1
-
-	del c
-	del k
-	print (syllables)
-	if len(syllables)==2:
-		for i in syllables[0]:
+	syl=syllables(word)
+	print (syl)
+	if len(syl)==2:
+		for i in syl[0]:
 			if i=='о' or i=='О' or 'ё' or 'Ё':
-				syllables[0]='хуё'
+				syl[0]='хуё'
 			elif i=='а' or i=='А':
-				syllables[0]='хуя'
+				syl[0]='хуя'
 			elif i=='у' or i=='У':
-				syllables[0]='хую'
+				syl[0]='хую'
 			elif i=='э' or i=='Э' or i=='е' or i=='Е':
-				syllables[0]='хуе'
+				syl[0]='хуе'
 			elif i=='Ы' or i=='Ы' or i=='и' or 'И':
-				syllables[0]='хуи'
+				syl[0]='хуи'
 	else:
-		if syllables[1]=='ишк':
-			syllables.pop(0)
-			syllables.insert(0, 'ху')
+		if syl[1]=='ишк':
+			syl[0]='ху'
 		else:
-			i=syllables[1][0]
+			i=syl[1][0]
 			if i=='о' or i=='О':
-				syllables[1]='ё'+syllables[1][1:]
+				syl[1]='ё'+syl[1][1:]
 			elif i=='а' or i=='А':
-				syllables[1]='я'+syllables[1][1:]
+				syl[1]='я'+syl[1][1:]
 			elif i=='у' or i=='У':
-				syllables[1]='ю'+syllables[1][1:]
+				syl[1]='ю'+syl[1][1:]
 			elif i=='э' or i=='Э' or i=='е' or i=='Е':
-				syllables[1]='е'+syllables[1][1:]
+				syl[1]='е'+syl[1][1:]
 			elif i=='Ы' or i=='Ы' or i=='и' or 'И':
-				syllables[1]='и'+syllables[1][1:]
-			syllables.pop(0)
-			syllables.insert(0, 'ху')
-
+				syl[1]='и'+syl[1][1:]
+			syl[0]='ху'
 	word=''
-	word = ''.join(syllables)
+	word = ''.join(syl)
 	print (word)
 	return word
