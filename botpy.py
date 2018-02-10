d67b88194b564d064e0e283a8703fe6a575ce3a7
Anton Veretennikov <stigmato135@gmail.com>
2018-02-10 20:44:01 +0300

    1

--- botpy.py
+++ botpy.py
@@ -1,39 +1,43 @@
 from syllables import syllables
+from syllables import vowels_list
 
 
 def substitution(word):
-	syl=syllables(word)
-	print (syl)
-	if len(syl)==2:
-		for i in syl[0]:
-			if i.lower()=='о'or i.lower()=='ё':
-				syl[0]='хуё'
-			elif i.lower()=='а':
-				syl[0]='хуя'
-			elif i.lower()=='у':
-				syl[0]='хую'
-			elif i.lower()=='э' or i.lower()=='е':
-				syl[0]='хуе'
-			elif i.lower()=='ы' or i.lower()=='и':
-				syl[0]='хуи'
-	else:
-		if syl[1]=='ишк':
-			syl[0]='ху'
-		else:
-			i=syl[1][-1]
-			if i.lower()=='о' or i.lower()=='ё':
-				syl[1]='ё'+syl[1][:-1]
-			elif i.lower()=='а':
-				syl[1]='я'+syl[1][:-1]
-			elif i=='у':
-				syl[1]='ю'+syl[1][:-1]
-			elif i.lower()=='э' or i.lower()=='е':
-				syl[1]='е'+syl[1][:-1]
-			elif i.lower()=='ы' or i.lower()=='и':
-				syl[1]='и'+syl[1][:-1]
-			syl[0]='ху'
-	word=''
-	word = ''.join(syl)
-	print (word)
-	return word
-	
+    syl = syllables(word)
+    print (syl)
+    if len(syl) == 2:
+        if syl[1][0] not in vowels_list:
+            for i in syl[0]:
+                if i.lower() == 'о' or i.lower() == 'ё':
+                    syl[0] = 'хуё'
+                elif i.lower() == 'а':
+                    syl[0] = 'хуя'
+                elif i.lower() == 'у':
+                    syl[0] = 'хую'
+                elif i.lower() == 'э' or i.lower() == 'е':
+                    syl[0] = 'хуе'
+                elif i.lower() == 'ы' or i.lower() == 'и':
+                    syl[0] = 'хуи'
+        else:
+            syl[0] = 'ху'
+    else:
+        if syl[1] == 'ишк':
+            syl[0] = 'ху'
+        else:
+            i = syl[0][-1]
+            print (i)
+            if i.lower() == 'о' or i.lower() == 'ё':
+                syl[1] = 'ё' + syl[1]
+            elif i.lower() == 'а':
+                syl[1] = 'я' + syl[1]
+            elif i == 'у':
+                syl[1] = 'ю' + syl[1]
+            elif i.lower() == 'э' or i.lower() == 'е':
+                syl[1] = 'е' + syl[1]
+            elif i.lower() == 'ы' or i.lower() == 'и':
+                syl[1] = 'и' + syl[1]
+            syl[0] = 'ху'
+    word = ''
+    word = ''.join(syl)
+    print (word)
+    return word
--- syllables.py
+++ syllables.py
@@ -1,5 +1,18 @@
 vowels_list=['а','и','е','ё','о','у','ы','э','ю','я',]
 
+'''
+def syllables(word):
+    syllables = []
+    current_syllable = ""
+    for letter in word:
+        current_syllable += letter
+        if letter in vowels_list:
+            syllables.append(current_syllable)
+            current_syllable = ""
+    if current_syllable !="":
+        syllables[-1] += current_syllable
+    return syllables
+'''
 
 def syllables(word):
     syllables = []
@@ -11,4 +24,12 @@ def syllables(word):
             current_syllable = ""
     if current_syllable !="":
         syllables[-1] += current_syllable
+    for id_syl,syl in enumerate(syllables):
+        current_syllable = ""
+        for id_letter,letter in enumerate(syl):
+            current_syllable += letter
+            if letter == "ъ" or letter == "ь":
+                current_syllable = syllables[id_syl - 1] + current_syllable
+                syllables[id_syl - 1] = current_syllable
+                syllables[id_syl] = syllables[id_syl][id_letter + 1:]
     return syllables
