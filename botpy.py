9a340fd006a7976030d23abfd031dddb08e94dfc
Anton Veretennikov <stigmato135@gmail.com>
2018-02-11 10:33:04 +0300

    гласная 2 слога

--- botpy.py
+++ botpy.py
@@ -1,10 +1,24 @@
-from syllables import syllables
-from syllables import vowels_list
+from syllables import syllables,consoles,vowels_list
 
+def bonding(syl):
+    i = syl[0][-1]
+    if i.lower() == 'о' or i.lower() == 'ё':
+        syl[1] = 'ё' + syl[1]
+    elif i.lower() == 'а':
+        syl[1] = 'я' + syl[1]
+    elif i == 'у':
+        syl[1] = 'ю' + syl[1]
+    elif i.lower() == 'э' or i.lower() == 'е':
+        syl[1] = 'е' + syl[1]
+    elif i.lower() == 'ы' or i.lower() == 'и':
+        syl[1] = 'и' + syl[1]
+    syl[0] = 'ху'
+    return syl
 
 def substitution(word):
     syl = syllables(word)
     print (syl)
+    print (consoles(word))
     if len(syl) == 2:
         if syl[1][0] not in vowels_list:
             for i in syl[0]:
@@ -21,23 +35,23 @@ def substitution(word):
         else:
             syl[0] = 'ху'
     else:
+        for id,letter in enumerate(syl[1]):
+            if letter in vowels_list:
+                new_id=id
+                i=letter
         if syl[1] == 'ишк':
             syl[0] = 'ху'
+        elif new_id!=0 :
+            syl=bonding(syl)
+        elif new_id==0 and len(syl[1])>1:
+            syl[1]=syl[1][1:]
+            syl=bonding(syl)
         else:
-            i = syl[0][-1]
-            print (i)
-            if i.lower() == 'о' or i.lower() == 'ё':
-                syl[1] = 'ё' + syl[1]
-            elif i.lower() == 'а':
-                syl[1] = 'я' + syl[1]
-            elif i == 'у':
-                syl[1] = 'ю' + syl[1]
-            elif i.lower() == 'э' or i.lower() == 'е':
-                syl[1] = 'е' + syl[1]
-            elif i.lower() == 'ы' or i.lower() == 'и':
-                syl[1] = 'и' + syl[1]
-            syl[0] = 'ху'
+            del syl[1]
+            syl=bonding(syl)
+
     word = ''
     word = ''.join(syl)
     print (word)
+
     return word
--- syllables.py
+++ syllables.py
@@ -1,18 +1,9 @@
 vowels_list=['а','и','е','ё','о','у','ы','э','ю','я',]
-
-'''
-def syllables(word):
-    syllables = []
-    current_syllable = ""
-    for letter in word:
-        current_syllable += letter
-        if letter in vowels_list:
-            syllables.append(current_syllable)
-            current_syllable = ""
-    if current_syllable !="":
-        syllables[-1] += current_syllable
-    return syllables
-'''
+consoles_list=['без','бес','в','во','вз','взо','вс','вне','внутри','воз','возо','вос','все','вы','до','за','из','изо',
+               'ис','испод','к','кое','кой','меж','междо','между','на','над','надо','наи','не','небез','небес','недо',
+               'ни','низ','низо','нис','о','об','обо','обез','обес','около','от','ото','па','пере','по','под','подo',
+               'поза','после','пра','пре','пред','предо','преди','при','про','противо','раз','разо','рас','роз','рос',
+               'с','со','сверх','среди','су','сыз','тре','у','чрез','через','черес']
 
 def syllables(word):
     syllables = []
@@ -33,3 +24,12 @@ def syllables(word):
                 syllables[id_syl - 1] = current_syllable
                 syllables[id_syl] = syllables[id_syl][id_letter + 1:]
     return syllables
+
+def consoles(word):
+    new_word=""
+    for id,letter in enumerate(word):
+        new_word+=letter
+        if new_word in consoles_list  and id<7:
+            word=word[id+1:]
+            break
+    return word
