8afaa1fcaf4e09b1c217b5386e36ec009df0fb7e
Anton Veretennikov <stigmato135@gmail.com>
2018-02-12 09:33:35 +0300

    подмена

--- botpy.py
+++ botpy.py
@@ -1,51 +1,44 @@
-from syllables import syllables,consoles,vowels_list
+from syllables import syllables,vowels_list
 
-def bonding(syl):
-    i = syl[0][-1]
-    if i.lower() == 'о' or i.lower() == 'ё':
-        syl[1] = 'ё' + syl[1]
-    elif i.lower() == 'а':
-        syl[1] = 'я' + syl[1]
-    elif i == 'у':
-        syl[1] = 'ю' + syl[1]
-    elif i.lower() == 'э' or i.lower() == 'е':
-        syl[1] = 'е' + syl[1]
-    elif i.lower() == 'ы' or i.lower() == 'и':
-        syl[1] = 'и' + syl[1]
+def bonding(syl, letter):
     syl[0] = 'ху'
+    if letter.lower() == 'ё':
+        syl[0] = syl[0]+'ё'
+    elif letter.lower() == 'а':
+        syl[0] = syl[0]+'я'
+    elif letter.lower() == 'у':
+        syl[0] = syl[0] + 'ю'
+    elif letter.lower() == 'э' or letter.lower() == 'е' or letter.lower() == 'о':
+        syl[0] = syl[0] + 'е'
+    elif letter.lower() == 'ы' or letter.lower() == 'и':
+        syl[0] = syl[0] + 'и'
     return syl
 
 def substitution(word):
     syl = syllables(word)
     print (syl)
-    print (consoles(word))
-    if len(syl) == 2:
+    length = len(syl)
+    if length == 5 or length == 6:
+        del syl[0]
+    if length == 7 or length == 8:
+        del syl[0:2]
+    for id, letter in enumerate(syl[0]):
+        if letter in vowels_list:
+            new_id = id
+            i = letter
+    if length == 2:
         if syl[1][0] not in vowels_list:
-            for i in syl[0]:
-                if i.lower() == 'о' or i.lower() == 'ё':
-                    syl[0] = 'хуё'
-                elif i.lower() == 'а':
-                    syl[0] = 'хуя'
-                elif i.lower() == 'у':
-                    syl[0] = 'хую'
-                elif i.lower() == 'э' or i.lower() == 'е':
-                    syl[0] = 'хуе'
-                elif i.lower() == 'ы' or i.lower() == 'и':
-                    syl[0] = 'хуи'
+            syl = bonding(syl, i)
         else:
             syl[0] = 'ху'
     else:
-        for id,letter in enumerate(syl[1]):
-            if letter in vowels_list:
-                new_id=id
-                i=letter
         if syl[1] == 'ишк':
             syl[0] = 'ху'
         elif new_id!=0 :
-            syl=bonding(syl)
+            syl=bonding(syl,i)
         elif new_id==0 and len(syl[1])>1:
             syl[1]=syl[1][1:]
-            syl=bonding(syl)
+            syl=bonding(syl,i)
         else:
             del syl[1]
             syl=bonding(syl)
